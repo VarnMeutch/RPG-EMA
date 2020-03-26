e1cc60af5c248a858d69baf8c2ee3496c1eea3ae
Shinrod <36998480+Shinrod@users.noreply.github.com>
2020-03-26 14:25:16 +0100

    Test MainMenu

--- core/src/com/RPG/game/screens/MainMenu.java
+++ core/src/com/RPG/game/screens/MainMenu.java
@@ -81,7 +81,7 @@ public class MainMenu implements Screen {
      */
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     /**
