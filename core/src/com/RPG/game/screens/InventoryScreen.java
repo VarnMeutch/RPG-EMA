05bdb6d35332c9874302ac1340435710e5e28d28
emmalacaille <Emma.Lacaille@mines-ales.org>
2020-03-31 20:21:49 +0200

    Inventaire et Item

--- core/src/com/RPG/game/screens/InventoryScreen.java
+++ core/src/com/RPG/game/screens/InventoryScreen.java
@@ -9,63 +9,14 @@ public class InventoryScreen implements Screen {
 
     // --- ATTRIBUTES --------------------------------------------------------------------------------------------------
     private RPGMain game;
-    private int PV;
-    private int pieces;
-    private int chapo;  //nombre de potions d'invisibilité que le joueur a à sa disposition
 
 
     // --- CONSTRUCTORS ------------------------------------------------------------------------------------------------
     public InventoryScreen(RPGMain game) {
         this.game = game;
-        PV=100;  //on commence le jeu à 100 PV (le maximum)
-        pieces=4;  //on commence le jeu avec 4 pièces
-        chapo=0;  //on commence le jeu avec O potions d'invisibilité
     }
 
     // --- METHODS -----------------------------------------------------------------------------------------------------
-
-    /**
-     * Cette méthode change le nombre de PV du perso.
-     * A modifier si on choisit plutôt de descendre ou de monter les PV d'une certaine valeur
-     * @param nouveauPV nouvelle valeur des PV
-     */
-    public void setPV(int nouveauPV) {
-        if ( (nouveauPV >= 0) && (nouveauPV <= 100) ) {
-            PV = nouveauPV;
-        }
-    }
-
-    /**
-     * Cette méthode augmente le nombre de pièces du perso.
-     * @param nouvellesPieces nombre de pièces supplémentaires
-     */
-    public void gagnerPieces (int nouvellesPieces) {
-        pieces=pieces+nouvellesPieces;
-    }
-
-    /**
-     * Cette méthode diminue le nombre de pièces du perso.
-     * On suppose que le perso a plus de pièces qu'il n'en perd.
-     * @param piecesDepensees nouvelle valeur des pièces
-     */
-    public void depenserPieces(int piecesDepensees) {
-        pieces=pieces-piecesDepensees;
-    }
-
-    /**
-     * Cette méthode augmente le nombre de potions d'invisibilité du perso.
-     */
-    public void gagnerChapo() {
-        chapo=chapo+1;
-    }
-
-    /**
-     * Cette méthode diminue le nombre de potions d'invisibilité du perso.
-     * On suppose que le perso a au moins 1 chapo.
-     */
-    public void depenserChapo() {
-        chapo=chapo-1;
-    }
 
     /**
      * Called when this screen becomes the current screen for a {@link Game}.
