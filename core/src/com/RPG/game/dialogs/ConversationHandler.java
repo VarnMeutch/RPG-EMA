cb13bf4a24648bec6d44b3568d83c880699b5cf0
jean-baptiste <Jean-Baptiste.Edouard@mines-ales.org>
2020-04-02 20:29:46 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	core/build/libs/core-1.0.jar
    #	core/src/com/RPG/game/dialogs/XmlReader.java

--- core/src/com/RPG/game/dialogs/ConversationHandler.java
+++ core/src/com/RPG/game/dialogs/ConversationHandler.java
@@ -8,15 +8,21 @@ public class ConversationHandler {
 
     private XmlReader reader;
     private Npc npc;
+    private Line [] convTab;
+
 
     public ConversationHandler(File file){
         reader=new XmlReader(file);
         npc=reader.read();
+        convTab = npc.getLineList();
 
     }
 
     public void test(){
         System.out.println(npc.getNpcName());
+        System.out.println("--------------");
+        Line tmp = convTab[0];
+        System.out.println(tmp.);
     }
 
 
--- core/src/com/RPG/game/dialogs/Options.java
+++ core/src/com/RPG/game/dialogs/Options.java
@@ -7,6 +7,22 @@ public class Options {
     private ArrayList<Option> optionList;
     private ArrayList<Text> textList;
 
+    public ArrayList<Option> getOptionList() {
+        return optionList;
+    }
+
+    public void setOptionList(ArrayList<Option> optionList) {
+        this.optionList = optionList;
+    }
+
+    public ArrayList<Text> getTextList() {
+        return textList;
+    }
+
+    public void setTextList(ArrayList<Text> textList) {
+        this.textList = textList;
+    }
+
     public Options(ArrayList<Option> optionList, ArrayList<Text> textList) {
         this.optionList = optionList;
         this.textList = textList;
