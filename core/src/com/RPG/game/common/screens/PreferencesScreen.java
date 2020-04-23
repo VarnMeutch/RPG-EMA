5df0dbd944b4d863e5f20a0201aedae48216c4b6
jean-baptiste <Jean-Baptiste.Edouard@mines-ales.org>
2020-04-23 09:18:40 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	core/build/libs/core-1.0.jar
    #	core/src/com/RPG/game/dialogs/XmlReader.java

--- core/src/com/RPG/game/common/screens/PreferencesScreen.java
+++ core/src/com/RPG/game/common/screens/PreferencesScreen.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -62,10 +63,12 @@ public class PreferencesScreen implements Screen {
      */
     @Override
     public void render(float delta) {
+
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         diag.activate();
         diag.chooseFile("Peter");
-        System.out.println("test");
-        diag.test();
 
         root.setFillParent(true);
         stage.addActor(root);
